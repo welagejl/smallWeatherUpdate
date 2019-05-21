687894680b1cfe14747d8601aa2fe2480f4b163c
welagejl <47874343+welagejl@users.noreply.github.com>
2019-05-21 03:01:31 -0300

    added headers to the url request bc 403 error

--- weather.py
+++ weather.py
@@ -4,11 +4,11 @@ import arcade
 import urllib.request
 import sys
 
-#windows constants
+#window constants
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
 
-#create window
+#create window/events
 class window(arcade.Window):
     def __init__(self,SCREEN_WIDTH,SCREEN_HEIGHT, title = "Today's Weather"):
         super().__init__(SCREEN_WIDTH,SCREEN_HEIGHT, title = "Today's Weather")
@@ -20,12 +20,12 @@ class window(arcade.Window):
         temp = getTemp()
         cond = getCond()
         
-        arcade.draw_text("Today's Temperature: ",200,350,color = arcade.color.RED)
-        arcade.draw_text(temp + ' F',200,300,color = arcade.color.RED)
-        arcade.draw_text(cond,200,250,color = arcade.color.RED)
+        arcade.draw_text("Today's Temperature: ",100,350,color = arcade.color.RED)
+        arcade.draw_text(temp + ' F',100,300,color = arcade.color.RED)
+        arcade.draw_text(cond,100,250,color = arcade.color.RED)
 
         #determine picture based on cond
-        if cond == 'Partly Cloudy' or  cond == 'Mostly Cloudy':
+        if cond == 'Partly Cloudy':
             image = arcade.Sprite("partly_cloudy.png")
             image.center_x = 200
             image.center_y = 150
@@ -60,7 +60,9 @@ def getTemp():
     i = 0
     while i == 0:
         webpage = "https://www.wunderground.com/weather/us/oh/cincinnati/45201"
-        websource = urllib.request.urlopen(webpage)
+        
+        web = urllib.request.Request(webpage, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"})
+        websource = urllib.request.urlopen(web)
         #allow for page to update
         time.sleep(3)
         
@@ -68,30 +70,22 @@ def getTemp():
         #look for current temp
         soup = soup.find_all('span', {'class', 'wu-value wu-value-to'},{'style', '"color:#93c124;"'})
         try:
-             temp = str(soup[1])
+             s = str(soup[1])
         except IndexError:
             i = 0
         else:
             i = 1
-        i = 0
-        x = 0
-        getTemp = ''
-        while temp[i] != '>':
-            x = i
-            i +=1
-        x = x + 2
-        while temp[x] !=  '<':
-            getTemp = getTemp + temp[x]
-            x += 1
             
-    return getTemp
+    return s[62:64]
 
 #Get the sky conditions
 def getCond():
     i = 0
     while i == 0:
         webpage = "https://www.wunderground.com/weather/us/oh/cincinnati/45201"
-        websource = urllib.request.urlopen(webpage)
+        
+        web = urllib.request.Request(webpage, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"})
+        websource = urllib.request.urlopen(web)
         #allow for page to update
         time.sleep(3)
             
